7e69b6378cd14ded40794198d82bf847b77c068c
Elvis Memic <elvis.memic@outlook.com>
2023-10-08 01:44:51 +0200

    Init project

---
+++ let_and_const.js
@@ -0,0 +1,34 @@
+
+let firstName = 'Elvis';
+const lastName = 'Memic';
+// lastName = 'Memija'; // assignment operation
+
+let age = 41;
+let birthDate = new Date();
+
+let person = {
+    firstName: 'Harun',
+    lastName: 'Memic'
+}
+
+let duplicate = function(value){
+    return value * 2;
+}
+
+const person2 = {
+    firstName: 'Amar',
+    lastName: 'Memic'
+}
+
+person2.firstName = 'Esmar'; // we are not reassigning person2
+
+// person2 = {
+
+// } // can't do this
+
+
+console.log(person2);
+
+for (var x = 1; x <= 10; x++){}
+
+console.log(x);
